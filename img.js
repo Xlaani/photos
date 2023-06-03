a441dbf84491046eadee772246e62a8f36e64a4b
DevCorvus <devcorvus.contact@gmail.com>
2023-06-03 19:26:05 -0400

    add .prettierrc, format code and fix some variables/typos

--- img.js
+++ img.js
@@ -1,30 +1,33 @@
 async function getPhotos() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/photos");
-  const photos = await response.json();
-  return photos;
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const photos = await response.json();
+    return photos;
 }
 
-const buttom = document.getElementById("buttom");
-const content = document.getElementById("content");
-buttom.addEventListener("click", async () => {
-  photos = await getPhotos();
-  for (photo of photos) {
-    const div = document.createElement("div");
-    const p = document.createElement("p");
-    const img = document.createElement("img");
-    p.textContent = photo.title;
-    img.src = photo.url;
-    div.appendChild(p2);
-    div.appendChild(p);
-    div.appendChild(img);
-    content.appendChild(div);
-  }
+const buttom = document.getElementById('buttom');
+
+buttom.addEventListener('click', async () => {
+    const content = document.getElementById('content');
+
+    const photos = await getPhotos();
+    for (const photo of photos) {
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+        const img = document.createElement('img');
+
+        p.textContent = photo.title;
+        img.src = photo.url;
+
+        div.appendChild(p);
+        div.appendChild(img);
+        content.appendChild(div);
+    }
 });
 
-// TODO: crear boton para eliminar cada foto por id
+// TODO: crear botón para eliminar cada foto por id
 
-// TODO: capacidad de actualizar el titulo de cada foto
+// TODO: capacidad de actualizar el título de cada foto
 
-// TODO: barra de busqueda para encontrar fotos
+// TODO: barra de búsqueda para encontrar fotos
 
 // TODO: crear interfaz agradable a la vista

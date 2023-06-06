83ee53498069a74d8c85403961ee6e64970f0f10
DevCorvus <devcorvus.contact@gmail.com>
2023-06-06 17:24:16 -0400

    set id for parent div instead of child elements

--- img.js
+++ img.js
@@ -14,20 +14,21 @@ button.addEventListener('click', async () => {
     // Using fixed amount of photos temporarily because if not it'll turn into an XBOX 360 Red Ring of Death
     for (const photo of photos.slice(0, 100)) {
         const template = `
-            <div>
+            <div data-id="${photo.id}">
                 <p>${photo.title}</p>
                 <img src="${photo.url}" />
-                <button data-id="${photo.id}">Delete</button>
+                <button data-delete-btn>Delete</button>
             </div>
         `;
 
         content.insertAdjacentHTML('beforeend', template);
 
-        content
-            .querySelector(`[data-id="${photo.id}"]`)
-            .addEventListener('click', (event) => {
-                event.target.parentElement.remove();
-            });
+        const div = content.querySelector(`[data-id="${photo.id}"]`);
+
+        const deleteButton = div.querySelector('[data-delete-btn]');
+        deleteButton.addEventListener('click', () => {
+            div.remove();
+        });
     }
 });
 
